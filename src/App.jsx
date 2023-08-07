36e61a6387261024cacc002b36aa832f09900abf
nairut <turian@hotmail.com>
2023-08-07 17:01:50 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import { Celular } from "./screens/Celular";
+import { DesktopForm } from "./screens/DesktopForm";
+import { CellForm } from "./screens/CellForm";
+import { Main } from "./screens/Index/Main";
+
+import { Obrigado } from "./screens/Obrigado";
+import React, { useEffect } from "react";
+
+// Lazy-loaded components
+const LazyTagManager = React.lazy(() => import("./LazyTagManager"));
+
+
+export const App = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  const renderMainPage = () => {
+    if (isMobile) {
+      return <Celular />;
+    } else {
+      return <Main />;
+    }
+  };
+
+  const renderFormPage = () => {
+    if (isMobile) {
+      return <CellForm />;
+    } else {
+      return <DesktopForm />;
+    }
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/*",
+      element: renderMainPage(),
+    },
+    {
+      path: "/desktop-form",
+      element: renderFormPage(),
+    },
+
+
+    {
+      path: "/teste",
+      element: <Obrigado />,
+    },
+
+
+     
+  ]);
+
+
+
+  return (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      {/* Lazy-load and render the TagManager component */}
+      <LazyTagManager />
+      <RouterProvider router={router} />
+    </React.Suspense>
+  );
+
+};
+
+
+
---
+++ src/screens/Obrigado/Obrigado.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export const Obrigado = () => {
+  return (
+    <div className="frame">
+      <div className="h-1-wrapper">
+        <h1 className="text-wrapper">Obrigfffado!!!!!</h1>
+      </div>
+    </div>
+  );
+};
