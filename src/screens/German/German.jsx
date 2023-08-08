12543594a7fe1009c68de39b3a82286827e77b94
nairut <turian@hotmail.com>
2023-08-08 12:18:49 -0300

    Update German.jsx

--- src/screens/German/German.jsx
+++ src/screens/German/German.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import { Group } from "../../components/Group";
 import { Star16 } from "../../icons/Star16";
 import "./style.css";
+import MetaTags from 'react-meta-tags';
 
 export const German = () => {
   return (
     <div className="spanish">
+                <MetaTags>
+            <title>Page 1</title>
+            <meta id="meta-description" name="description" content="Some description." />
+            <meta id="og-title" property="og:title" content="MyApp" />
+            <meta id="og-image" property="og:image" content="path/to/image.jpg" />
+          </MetaTags>
+          <div class="content"> Some Content </div>
+          
       <div className="div-2">
         <div className="overlap-6">
           <div className="overlap-7">
