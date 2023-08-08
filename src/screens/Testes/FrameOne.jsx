43b3e6df1608eac6d6aa7c68351d12f43cc25cad
nairut <turian@hotmail.com>
2023-08-08 10:58:50 -0300

    Update FrameOne.jsx

--- src/screens/Testes/FrameOne.jsx
+++ src/screens/Testes/FrameOne.jsx
@@ -1,30 +1,37 @@
-import React, { useState, useRef } from 'react';
+import React from "react";
+import "./style.css";
 
-export const Testes = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const containerRef = useRef();
-  const imageUrls = [
-    'url_of_image_1.jpg',
-    'url_of_image_2.jpg',
-    // Add the URLs of the rest of your images here
-  ];
-  const imageChangeThreshold = 200; // Adjust this value to set the scroll threshold for image change
-
-  const handleScroll = () => {
-    const { scrollTop } = containerRef.current;
-    const newIndex = Math.floor(scrollTop / imageChangeThreshold);
-    if (newIndex !== currentImageIndex) {
-      setCurrentImageIndex(newIndex);
-    }
-  };
-
+export const FrameOne = () => {
   return (
-    <div
-      ref={containerRef}
-      onScroll={handleScroll}
-      style={{ overflowY: 'scroll', height: '400px' }} // Adjust the height and overflow property as needed
-    >
-      <img src={imageUrls[currentImageIndex]} alt={`Image ${currentImageIndex + 1}`} />
+    <div className="frame-one">
+      <div className="div">
+
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+        <img className="keyboard" alt="Keyboard" src="/img/group-8-4.png" />
+
+        <div className="scrollbar" />
+      </div>
     </div>
   );
 };
