652fe633a7d90c7be7c246a13542ff59b3a081d0
nairut <turian@hotmail.com>
2023-08-10 14:03:38 -0300

    Update DesktopFormPT.jsx

--- src/screens/DesktopFormPT/DesktopFormPT.jsx
+++ src/screens/DesktopFormPT/DesktopFormPT.jsx
@@ -16,7 +16,7 @@ export const DesktopFormPT = () => {
 
         <div className="group">
           <button className="overlap-group" type="submit">
-            <div className="text-wrapper">Send</div>
+            <div className="text-wrapper">Enviar</div>
           </button>
         </div>
 
@@ -30,7 +30,7 @@ export const DesktopFormPT = () => {
           <div className="overlap-wrapper">
             <div className="overlap-2">
               <div className="rectangle-2" />
-              <div className="text-wrapper-2">Message</div>
+              <div className="text-wrapper-2">Mensagem</div>
             </div>
           </div>
 
@@ -41,7 +41,7 @@ export const DesktopFormPT = () => {
 
           <div className="overlap-group-wrapper">
             <div className="div-wrapper">
-              <div className="text-wrapper-3">Email</div>
+              <div className="text-wrapper-3">E-mail</div>
             </div>
           </div>
         </div>
@@ -53,7 +53,7 @@ export const DesktopFormPT = () => {
 
           <div className="group-2">
             <div className="overlap-5">
-              <div className="text-wrapper-4">Last name</div>
+              <div className="text-wrapper-4">Sobrenome</div>
             </div>
           </div>
         </div>
@@ -66,7 +66,7 @@ export const DesktopFormPT = () => {
 
           <div className="group-3">
             <div className="overlap-5">
-              <div className="text-wrapper-5">First name</div>
+              <div className="text-wrapper-5">Nome</div>
             </div>
           </div>
         </div>
@@ -77,12 +77,11 @@ export const DesktopFormPT = () => {
         <img className="line" alt="Line" src="/img/line-1.png" />
         <Lady className="lady-instance" />
         <img className="magma-translation" alt="Magma translation" src="/img/magma-translation-png.png" />
-        <h1 className="h-1">Get a Free, No-Obligation Price Quote</h1>
+        <h1 className="h-1">Receba um Orçamento Gratuito</h1>
         <p className="p">
-          MAGMA is a global localization solutions provider, enabling brands to establish an authentic presence in
-          diverse markets around the world.
+        MAGMA é um fornecedor global de soluções de localização, permitindo que marcas estabeleçam uma presença autêntica em diversos mercados ao redor do mundo.
         </p>
-        <p className="text-wrapper-6">Magma Translation | All rights reserved. ©Magma™</p>
+        <p className="text-wrapper-6">Magma Translation | Todos os direitos reservados. ©Magma™</p>
         <img className="img" alt="Magma translation" src="/img/magma-translation-png-1.png" />
       </div>
     </div>
