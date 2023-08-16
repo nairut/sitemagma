40d1c3b031074122019b8b45cc1aad4d16c21e53
nairut <turian@hotmail.com>
2023-08-16 14:11:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,37 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route, ErrorBoundary  } from "react-router-dom";
-import { useMediaQuery } from "react-responsive";
-import { Celular } from "./screens/Celular";
-import { DesktopForm } from "./screens/DesktopForm";
-import {  Main } from "./screens/Main";
-import React, { useEffect } from "react";
-import { Depoimentos } from "./screens/Depoimentos";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Main } from "./screens/Main";
 import { HomePt } from "./screens/HomePt/HomePt";
-import { CelularPt } from "./screens/CelularPt";
-import { ContagemPalavra, Testes } from "./screens/Testes";
-import { OrcamentoPt } from "./screens/OrcamentoPt";
 
-
-
-
- 
-// Lazy-loaded components
-const LazyTagManager = React.lazy(() => import("./LazyTagManager"));
-
-
-export const App = () => {
-
-  
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main />,
-      loader: rootLoader,
-      children: [
-        {
-          path: "pt",
-          element: <HomePt />,
-          loader: teamLoader,
-        },
-      ],
-    },
-  ]);
-  
-
-
-
+const App = () => {
   return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      {/* Lazy-load and render the TagManager component */}
-      <LazyTagManager />
-      <RouterProvider router={router} />
-    </React.Suspense>
-  );
 
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path="/" 
+            element={<Main />} 
+          />
+          
+          <Route 
+            path="/team"
+            element={<HomePt />}
+          />
+        </Routes>
+      </BrowserRouter>
+    
+
+
+
+
+
+
+
+
+
+  );
 };
 
-
-
-
-
-
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
