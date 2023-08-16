a16976e9f40c9c67da28ccb1dde092080b4a5e89
nairut <turian@hotmail.com>
2023-08-16 14:20:14 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,50 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { Main } from "./screens/Main";
+import { createBrowserRouter, RouterProvider, BrowserRouter, Routes, Route } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import { Celular } from "./screens/Celular";
+import { DesktopForm } from "./screens/DesktopForm";
+import {  Main } from "./screens/Main";
+import React, { useEffect } from "react";
+import { Depoimentos } from "./screens/Depoimentos";
 import { HomePt } from "./screens/HomePt/HomePt";
+import { CelularPt } from "./screens/CelularPt";
+import { ContagemPalavra, Testes } from "./screens/Testes";
+import { OrcamentoPt } from "./screens/OrcamentoPt";
 
-const App = () => {
+
+
+
+ 
+// Lazy-loaded components
+const LazyTagManager = React.lazy(() => import("./LazyTagManager"));
+
+
+export const App = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+
+
   return (
 
-      <BrowserRouter>
-        <Routes>
-          <Route 
-            path="/" 
-            element={<Main />} 
-          />
-          
-          <Route 
-            path="/team"
-            element={<HomePt />}
-          />
-        </Routes>
-      </BrowserRouter>
-
-
+    <BrowserRouter>
+    <Routes>
+      <Route 
+        path="/" 
+        element={<Main />} 
+      />
+      
+      <Route 
+        path="/team"
+        element={<HomePt />}
+      />
+    </Routes>
+  </BrowserRouter>
   );
+
 };
 
+
 export default App;
+
+
+
