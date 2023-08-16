a1652addd991bf65cefe2c17a98816969e884b5d
nairut <turian@hotmail.com>
2023-08-16 10:29:22 -0300

    novo de novo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import { Celular } from "./screens/Celular";
 import { DesktopForm } from "./screens/DesktopForm";
@@ -10,13 +10,9 @@ import { HomePt } from "./screens/HomePt/HomePt";
 import { CelularPt } from "./screens/CelularPt";
 import { ContagemPalavra, Testes } from "./screens/Testes";
 import { OrcamentoPt } from "./screens/OrcamentoPt";
-import { Route, Routes } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
 
 
 
-
- 
 // Lazy-loaded components
 const LazyTagManager = React.lazy(() => import("./LazyTagManager"));
 
@@ -26,33 +22,182 @@ export const App = () => {
 
 
 
+  const router = createBrowserRouter([
+    
+      {
+        path: "/",
+        element: isMobile ? <Celular /> : <Main />,
+      },
+
+
+      {
+        path: "/desktop-form",
+        element: isMobile ? <Celular /> : <DesktopForm />,
+      },
+
+
+      //////////////////////Abaixo tudo de Português//////////////////////////////
+
+      {
+        path: "/pt",
+        element: isMobile ? <CelularPt /> : (
+          <HomePt
+            titulo="Serviço de Tradução Profissional"
+            textSize="large"
+            primeiraFrase="120 Idiomas que cabem no seu bolso"
+            segundaFrase="Traduzimos em mais de 60 áreas"
+            terceiraFrase="Linguistas com Mestrado"
+          />
+        ),
+
+      },
+
+
+            {path:"/pt/depoimentos",
+      element: isMobile ? <Celular /> :<Depoimentos/>,
+      },
+
+
+      {
+        path: "/ingles",
+        element: isMobile ? <CelularPt /> : (
+          <HomePt
+            titulo="Serviço de Tradução de Inglês"
+            textSize="large"
+            primeiraFrase="120 Idiomas que cabem no seu bolso"
+            segundaFrase="Traduzimos em mais de 60 áreas"
+            terceiraFrase="Linguistas com Mestrado"
+          />
+        ),
+      },
+
+      {
+        path: "/ingles/teste",
+        element: isMobile ? <CelularPt /> : (
+          <HomePt
+            titulo="testetestestestestset"
+            textSize="large"
+            primeiraFrase="120 Idiomas que cabem no seu bolso"
+            segundaFrase="Traduzimos em mais de 60 áreas"
+            terceiraFrase="Linguistas com Mestrado"
+          />
+        ),
+      },
+
+
+
+
+      {
+        path: "pt/servicos/traducao-espanhol",
+        element: isMobile ? <CelularPt /> : (
+          <HomePt
+            titulo="Serviço de Tradução de Espanhol"
+            textSize="large"
+            primeiraFrase="120 Idiomas que cabem no seu bolso"
+            segundaFrase="Traduzimos em mais de 60 áreas"
+            terceiraFrase="Linguistas com Mestrado"
+          />
+        ),
+      },
+
+
+
+      {
+        path: "/pt/servicostraducao-ingles-portugues",
+        element: isMobile ? <CelularPt /> : (
+          <HomePt
+            titulo="Tradução de Inglês para Português"
+            textSize="large"
+            primeiraFrase="120 Idiomas que cabem no seu bolso"
+            segundaFrase="Traduzimos em mais de 60 áreas"
+            terceiraFrase="Linguistas com Mestrado"
+          />
+        ),
+      },
+
+
+
+      {
+        path: "/pt/servicos/traducao-espanhol-portugues",
+        element: isMobile ? <CelularPt /> : (
+          <HomePt
+            titulo="Tradução de Espanhol para Português"
+            textSize="medium"
+            primeiraFrase="120 Idiomas que cabem no seu bolso"
+            segundaFrase="Traduzimos em mais de 60 áreas"
+            terceiraFrase="Linguistas com Mestrado"
+          />
+        ),
+      },
+
+
+
+
+
+
+////////////Páginas de Formulário em PT
+
+
+{
+  path: "/pt/orcamento",
+  element: isMobile ? <Celular /> : <OrcamentoPt
+  
+  titulo="Orçamento Grátis"
+  
+  />,
+},
+
+
+
+
+
+////////////Páginas de Promoções em PT1
+
+{
+  path: "/pt/promocoes/cupon",
+  element: isMobile ? <CelularPt /> : (
+    <HomePt
+      titulo="Serviço de Tradução Profissional"
+      textSize="large"
+      textCollor="large"
+      primeiraFrase={<span style={{ fontSize: '23px', color: 'red' }}>Cupom: 15% Off no Cupon DESCONTO15</span>}
+      segundaFrase='Insira o cupon DESCONTO15 na mensagem'
+      terceiraFrase="que você receberá a promoção"
+      textoBotao="Receba a promoção"
+    />
+  ),
+},
+
+
+
+
+
+
+
+
+
+      /////////////////Abaixo tudo de teste ////////////////////////////////
+
+      {
+        path: "/testes",
+        element: isMobile ? <Celular /> : <Testes />,
+      },
+
+
+
+ 
+
+  ]);
+
+
 
   return (
-
-    <BrowserRouter >
-    <Routes>
-
-    <Route path="/" element={isMobile ? <Celular /> : <Main />} >
-    </Route >
-      <Route path="pt" element={isMobile ? <Celular /> : <Testes />} >
-        <Route path="home" element={isMobile ? <Celular /> : <HomePt />} />
-      </Route >
-    {/* Other routes can be defined here */}
-
-
-    </Routes>
-
-
-  </BrowserRouter>
-
-
-);
-
-
-
-
-
-
+    <React.Suspense fallback={<div>Loading...</div>}>
+      {/* Lazy-load and render the TagManager component */}
+      <LazyTagManager />
+      <RouterProvider router={router} />
+    </React.Suspense>
+  );
 
 };
 
