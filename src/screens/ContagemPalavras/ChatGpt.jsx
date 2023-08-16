9e7ca4ffeea1818f9b303e2a28f94b5140cca7e5
nairut <turian@hotmail.com>
2023-08-16 20:00:31 -0300

    Update ChatGpt.jsx

--- src/screens/ContagemPalavras/ChatGpt.jsx
+++ src/screens/ContagemPalavras/ChatGpt.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const API_ENDPOINT = 'https://api.openai.com/v1/chat/completions';
-const API_KEY = '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'; // Make sure to handle this securely.
+const API_KEY = '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'; // Make sure to handle this securely.
 
 export function ChatGpt() {
     const [inputText, setInputText] = useState("");
